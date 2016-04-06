5d9bd44977a6ed56b34f0edc5e25f042addeff87
Michal Matlega <m1sz3lek@gmail.com>
2016-04-06 22:01:00 +0200

    Basic Task List

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
